b25b4f9494bccde0712d4201d2c8de1f492e0d90
niichtsShaiaz <niichtsShaiaz@users.noreply.github.com>
2020-01-10 00:51:10 +0100

    yes

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
 # dotfiles
+## Windows 10
++ **PowerShell core**
++ **Hyper**
++ **Firefox**
++ **Discord**
++ **Explorer**
+
+I'm using the colour scheme from [Elenapan's](https://github.com/elenapan/dotfiles) Ephemeral theme, Main background colour is  ``#344055``
+
+These links might be outdated.
+## PowerShell
+
+## Hyper
+
+[I used this as reference](https://tjay.dev/howto-my-terminal-shell-setup-hyper-js-zsh-starship/)
+
+Everything is in ``.hyper.js``  I have changed font size and added a couple plugins
+
+```
+plugins: [
+  'hyperpower',
+  'gitrocket',
+  "hyper-font-ligatures",
+  "hyper-one-dark",
+  "hypercwd",
+  "hyper-active-tab"
+],
+```
+
+I have changed the ``hyper-one-dark`` background colour to match my explorer theme.
+
+The config has been changed
+
+```
+shell: 'wsl.exe', # Make hyper not crash when closing in bash
+shellArgs: [], # ^same as above
+webGLRenderer: false, # Allows cool characters
+```
+![coolchars](./Hyper/Coolchars.png "Coolchars")
+
+
+## Firefox
+Modified [AnubisZ9's](https://github.com/AnubisZ9/python-projects/blob/master/Dotfiles/) Firefox css to be compatible with windows and changed some colours
+
+## Discord
+
+## Explorer
+To use custom themes on windows, you need to patch system files with [UltraUXThemePatcher](https://www.syssel.net/hoefs/software_uxtheme.php?lang=en)    
+
+Using the theme from the theme folder,``love lace darkest`` Love Lace made by [Niivu](https://www.deviantart.com/niivu)
+
+Using ``Lumicons`` from [Here](https://www.deviantart.com/vantler/art/Lumicons-662277185) Or [Here](https://www.deviantart.com/niivu/art/Lumicons-Installer-812994066)
+
+Using [T-Clock](https://github.com/White-Tiger/T-Clock) with custom clock string ``""dddd, mmm-dd h:nn tt""``
+
+Removing ribbon with [Ribbon Disabler](https://winaero.com/blog/ribbon-disabler-4-0-for-windows-10-version-1903/)
---
+++ Windows/Hyper/.hyper.js
@@ -0,0 +1,172 @@
+// Future versions of Hyper may add additional config options,
+// which will not automatically be merged into this file.
+// See https://hyper.is#cfg for all currently supported options.
+
+module.exports = {
+  config: {
+    // choose either `'stable'` for receiving highly polished,
+    // or `'canary'` for less polished but more frequent updates
+    updateChannel: 'stable',
+
+    // default font size in pixels for all tabs
+    fontSize: 14,
+
+    // font family with optional fallbacks
+    fontFamily: '"Fira code", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+
+    // default font weight: 'normal' or 'bold'
+    fontWeight: 'normal',
+
+    // font weight for bold characters: 'normal' or 'bold'
+    fontWeightBold: 'bold',
+
+    // line height as a relative unit
+    lineHeight: 1,
+
+    // letter spacing as a relative unit
+    letterSpacing: 0,
+
+    // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
+    cursorColor: 'rgba(248,28,229,0.8)',
+
+    // terminal text color under BLOCK cursor
+    cursorAccentColor: '#000',
+
+    // `'BEAM'` for |, `'UNDERLINE'` for _, `'BLOCK'` for █
+    cursorShape: 'BLOCK',
+
+    // set to `true` (without backticks and without quotes) for blinking cursor
+    cursorBlink: false,
+
+    // color of the text
+    foregroundColor: '#fff',
+
+    // terminal background color
+    // opacity is only supported on macOS
+    backgroundColor: '#000',
+
+    // terminal selection color
+    selectionColor: 'rgba(248,28,229,0.3)',
+
+    // border color (window, tabs)
+    borderColor: '#333',
+
+    // custom CSS to embed in the main window
+    css: '',
+
+    // custom CSS to embed in the terminal window
+    termCSS: '',
+
+    // if you're using a Linux setup which show native menus, set to false
+    // default: `true` on Linux, `true` on Windows, ignored on macOS
+    showHamburgerMenu: '',
+
+    // set to `false` (without backticks and without quotes) if you want to hide the minimize, maximize and close buttons
+    // additionally, set to `'left'` if you want them on the left, like in Ubuntu
+    // default: `true` (without backticks and without quotes) on Windows and Linux, ignored on macOS
+    showWindowControls: '',
+
+    // custom padding (CSS format, i.e.: `top right bottom left`)
+    padding: '12px 14px',
+
+    // the full list. if you're going to provide the full color palette,
+    // including the 6 x 6 color cubes and the grayscale map, just provide
+    // an array here instead of a color map object
+    colors: {
+      black: '#344055 ',
+      red         : '#e06c75', // red
+      green       : '#98c379', // green
+      yellow      : '#d19a66', // yellow
+      blue        : '#56b6c2', // blue
+      magenta     : '#c678dd', // pink
+      cyan        : '#56b6c2', // cyan
+      white       : '#d0d0d0', // light gray
+      lightBlack  : '#808080', // medium gray
+      lightRed    : '#e06c75', // red
+      lightGreen  : '#98c379', // green
+      lightYellow : '#d19a66', // yellow
+      lightBlue   : '#56b6c2', // blue
+      lightMagenta: '#c678dd', // pink
+      lightCyan   : '#56b6c2', // cyan
+      lightWhite  : '#ffffff', // white
+    },
+
+    // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
+    // if left empty, your system's login shell will be used by default
+    //
+    // Windows
+    // - Make sure to use a full path if the binary name doesn't work
+    // - Remove `--login` in shellArgs
+    //
+    // Bash on Windows
+    // - Example: `C:\\Windows\\System32\\bash.exe`
+    //
+    // PowerShell on Windows
+    // - Example: `C:\\WINDOWS\\System32\\WindowsPowerShell\\v1.0\\powershell.exe`
+    //shell: 'wsl.exe',
+    //shell: "C:\\Users\\Shiina\\AppData\\Local\\Packages\\CanonicalGroupLimited.Ubuntu18.04onWindows_79rhkp1fndgsc\\LocalState\rootfs\\bin",
+    shell: 'wsl.exe',
+    shellArgs: [],
+
+    // for setting shell arguments (i.e. for using interactive shellArgs: `['-i']`)
+    // by default `['--login']` will be used
+    //shellArgs: ['--login'],
+
+    // for environment variables
+    env: {},
+
+    // set to `false` for no bell
+    bell: 'SOUND',
+
+    // if `true` (without backticks and without quotes), selected text will automatically be copied to the clipboard
+    copyOnSelect: false,
+
+    // if `true` (without backticks and without quotes), hyper will be set as the default protocol client for SSH
+    defaultSSHApp: true,
+
+    // if `true` (without backticks and without quotes), on right click selected text will be copied or pasted if no
+    // selection is present (`true` by default on Windows and disables the context menu feature)
+    quickEdit: false,
+
+    // choose either `'vertical'`, if you want the column mode when Option key is hold during selection (Default)
+    // or `'force'`, if you want to force selection regardless of whether the terminal is in mouse events mode
+    // (inside tmux or vim with mouse mode enabled for example).
+    macOptionSelectionMode: 'vertical',
+
+    // URL to custom bell
+    // bellSoundURL: 'http://example.com/bell.mp3',
+
+    // Whether to use the WebGL renderer. Set it to false to use canvas-based
+    // rendering (slower, but supports transparent backgrounds)
+    webGLRenderer: false,
+
+    // for advanced config flags please refer to https://hyper.is/#cfg
+    activeTab: '🚀',
+  },
+
+  // a list of plugins to fetch and install from npm
+  // format: [@org/]project[#version]
+  // examples:
+  //   `hyperpower`
+  //   `@company/project`
+  //   `project#1.0.1`
+  plugins: [
+    'hyperpower',
+    'gitrocket',
+    //'hyper-statusline',
+    "hyper-font-ligatures",
+    "hyper-one-dark",
+    "hypercwd",
+    "hyper-active-tab"
+  ],
+
+  // in development, you can create a directory under
+  // `~/.hyper_plugins/local/` and include it here
+  // to load it and avoid it being `npm install`ed
+  localPlugins: [],
+
+  keymaps: {
+    // Example
+    // 'window:devtools': 'cmd+alt+o',
+  },
+};
